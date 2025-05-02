3843870ad1ed9edc33343b478869e26d36fd49b2
Chris Tomblin <ctomblin01@gmail.com>
2025-05-02 14:52:03 -0400

    remove mangohud

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux steam-devices mangohud goverlay
+dnf5 install -y steam-devices
 
 # Use a COPR Example:
 #
